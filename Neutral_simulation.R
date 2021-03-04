cc302d133d4053cfb41b0f549599c8175fa0542c
falko-buschke <falko-buschke@users.noreply.github.com>
2021-03-04 10:49:18 +0200

    Update Neutral_simulation.R

--- Neutral_simulation.R
+++ Neutral_simulation.R
@@ -133,26 +133,30 @@ png(filename="Fig2.png",width=21,height=20,units="cm",res=300)
 par(mai=c(0.65,0.72,0.05,0.12))
 par(mfrow=c(2,2))
 
-# First, create a blank plot of Extinction probability over (log) abundance.
+#####################
+#                   #
+#     Panel A       #
+#                   #
+#####################
 
+# First, create a blank plot of Extinction probability over (log) abundance.
 plot(0,0,type="n",las=1,xlim=c(1,400),log="x",
      ylim=c(0,1.1),ylab="Extinction probability",xlab="Abundance", 
      cex.axis=1.0, cex.lab= 1.3, mgp=c(2.4,0.6,0))
-# Add baseline
+# Label the panel
 mtext("(a)",cex=1.5, side = 3, adj = 0.075, line = -2,font=2)
-legend("topright",
-	pch=c(1,15,16,NA,NA,NA), lty=c(1,1,2,1,1,1),
+# Add a legend
+legend("topright",pch=c(1,15,16,NA,NA,NA), lty=c(1,1,2,1,1,1),
 	col=c("black","grey", "black", "yellow", "orange", "red"),
 	c("Extinction 100 years","Extinction 20 years","Extinction 10 years",
 		"VU threshold", "EN threshold", "CR theshold"), bty="n", cex=0.95)
 
-
-
+# Add the points for extinction probability within 100, 20, 10 years
 points(VU$d,VU$VU, type="o", col="black", lty=1, cex=1)
 points(EN$d,EN$EN, type="o", col="grey", pch=15, cex=1)
 points(CR$d,CR$CR, type="o", col="black", pch=16, lty=2,cex=1)
 
-
+# Add the lines showing the thresholds for Critically Endnagered, Endangered, and Vulnerable
 lines(c(1e-99,CR$d[max(which(CR$CR>=0.5))]),c(0.5,0.5), col="red")
 lines(rep(CR$d[max(which(CR$CR>=0.5))],2),
 	c(-1,CR$CR[max(which(CR$CR>=0.5))]), col="red",lty=2)
@@ -165,99 +169,116 @@ lines(c(1e-99,VU$d[max(which(VU$VU>=0.1))]),c(0.1,0.1), col="yellow")
 lines(rep(VU$d[max(which(VU$VU>=0.1))],2),
 	c(-1,VU$VU[max(which(VU$VU>=0.1))]), col="yellow",lty=2)
 
-
 CR.t <- CR$d[max(which(CR$CR>=0.5))]
 EN.t <- EN$d[max(which(EN$EN>=0.2))]
 VU.t <- VU$d[max(which(VU$VU>=0.1))]
 
+
+#####################
+#                   #
+#     Panel B       #
+#                   #
+#####################
+
+# First, create a blank plot of population size over time.
 plot(0,0, type="n", xlim=c(1970,2020), ylim=c(0,max(comm)),
  xlab="Year",las=1, ylab="Population size",
 	cex.axis=1.0, cex.lab= 1.3, mgp=c(2.4,0.6,0))
 	
-
+# Add background polygons to show the thresholds between extinction risk cateories
 polygon(c(1960,1960,2300,2300), c(0,CR.t,CR.t,0),col=rgb(1,0,0,0.25),border=NA)	
 polygon(c(1960,1960,2300,2300), c(CR.t,EN.t,EN.t,CR.t),col=rgb(1,0.3,0,0.1),border=NA)	
 polygon(c(1960,1960,2300,2300), c(EN.t,VU.t,VU.t,EN.t),col=rgb(1,1,0,0.1),border=NA)	
 polygon(c(1960,1960,2300,2300), c(VU.t,350,350,VU.t),col=rgb(0,0.6,0,0.1),border=NA)	
 
-
-
+# Run a loop to add each population one by one
 for (k in 1:dim(comm)[1]){
 	lines(years,comm[k,], col=rgb(0,0,0,0.5))
 }
-
+# Label the panel
 mtext("(b)",cex=1.5, side = 3, adj = 0.075, line = -2,font=2)
 
+#####################
+#                   #
+#     Panel C       #
+#                   #
+#####################
 
-
-
+# Create a matrix that assigns dummy codes to each threat category
 rli.mat <- ifelse(comm>VU.t,1,ifelse(comm>EN.t,0.6,ifelse(comm>CR.t,0.4,ifelse(comm>0,0.2,1e-99))))
 
-rli <- apply(rli.mat,2,mean)
-rli.conf <- apply(rli.mat,2,function(x){sd(x)/sqrt(length(x))})
-
-
+# Create a matrix for the percentage of species in each threat category
 prop.mat <- matrix(0,ncol=length(years), nrow=5)
 colnames(prop.mat) <- years; rownames(prop.mat) <- c("LC", "VU","EN", "CR", "EX")
 
+# Run a loop to calcualte the percentage of each species per threat category
 for (j in 1:dim(rli.mat)[2]){
-
-prop.mat[1,j] <- length(which(rli.mat[,j]==1))/length(rli.mat[,j]) *100
-prop.mat[2,j] <- length(which(rli.mat[,j]==0.6))/length(rli.mat[,j]) *100
-prop.mat[3,j] <- length(which(rli.mat[,j]==0.4))/length(rli.mat[,j]) *100
-prop.mat[4,j] <- length(which(rli.mat[,j]==0.2))/length(rli.mat[,j]) *100
-prop.mat[5,j] <- length(which(rli.mat[,j]<0.2))/length(rli.mat[,j]) *100
+	prop.mat[1,j] <- length(which(rli.mat[,j]==1))/length(rli.mat[,j]) *100
+	prop.mat[2,j] <- length(which(rli.mat[,j]==0.6))/length(rli.mat[,j]) *100
+	prop.mat[3,j] <- length(which(rli.mat[,j]==0.4))/length(rli.mat[,j]) *100
+	prop.mat[4,j] <- length(which(rli.mat[,j]==0.2))/length(rli.mat[,j]) *100
+	prop.mat[5,j] <- length(which(rli.mat[,j]<0.2))/length(rli.mat[,j]) *100
 }
 
-
+# Assign colours for each threat category
 iucn.col <- c("green", "yellow","orange", "red", "black")
+# Cumulate the percentages for the stacked bar chart
 cum.mat <- apply(prop.mat,2,cumsum)
 
-
-
-
+# Create a function that plots the changing percentages as steps, rather than straight lines
 stepX <- function(x){
 	x2 <- c(x[1],rep(x,each=2)[-1],1+x[length(x)], 1+x[length(x)])
 	x2
 }
-
 stepY <- function(y){
 	y2 <- c(0,rep(y, each=2),0)
 	y2
 }
 
+# First, create a blank plot of percentage threat categories over time.
 plot(years,rli, type="n", xlim=c(1970,2020), ylim=c(0,110),
  xlab="Year",las=1, ylab="Pecentage of species (%)",
 	cex.axis=1.0, cex.lab= 1.3, mgp=c(2.4,0.6,0)) 
-
+# Add the cumulative stepped polygons
 polygon(c(years,max(years+1),max(years)+1,rev(years)),c(rep(100,1+length(years)),rep(0,1+length(years))),col="black", border=NA)
 polygon(stepX(years),stepY(cum.mat[4,]),col="red", border="grey")
 polygon(stepX(years),stepY(cum.mat[3,]),col="orange", border="grey")
 polygon(stepX(years),stepY(cum.mat[2,]),col="yellow", border="grey")
 polygon(stepX(years),stepY(cum.mat[1,]),col="green", border="grey")
 
+# Add gret lines to give the illusion of seperate bars
 for(i in 1:length(years)){
 	lines(c(years[i],years[i]),c(0,100), col="grey")
 }
 
+# Add legend and label plot
 legend(1975,15, bg="white",pch=15, col=iucn.col, c("LC","VU","EN","CR", "EX"),horiz=T)
 mtext("(c)",cex=1.5, side = 3, adj = 0.075, line = -2,font=2)
 
 
+#####################
+#                   #
+#     Panel D       #
+#                   #
+#####################
 
+# Calculate the mean and standard error for the dummy variable of threat categories: Red List Index
+rli <- apply(rli.mat,2,mean)
+rli.conf <- apply(rli.mat,2,function(x){sd(x)/sqrt(length(x))})
 
-
-
+# First, create a blank plot of Red List Index over time.
 plot(years,rli, type="n", xlim=c(1970,2020), ylim=c(0,1.1),
  xlab="Year",las=1, ylab="Red List Index",
 	cex.axis=1.0, cex.lab= 1.3, mgp=c(2.4,0.6,0))
-
+# Add a polygon for confidence intervals
 polygon(c(years,rev(years)), c(rli+rli.conf,rev(rli-rli.conf)),col=rgb(0.4,0,0,0.3),border=NA)	
-
+# Add mean line
 lines(years,rli,col="white")
 
+# Label panel
 mtext("(d)",cex=1.5, side = 3, adj = 0.075, line = -2,font=2)
 
+# Close and save the plot device
 dev.off()
 
 
